62f1d89f16e893411068a7bff2e9050f300090dc
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-08-30 23:40:33 +0100

    implement list command

---
+++ cmd/list.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/s1ntaxe770r/templit/models"
+	"github.com/s1ntaxe770r/templit/utils"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list templates",
+	Long: `outputs a table of all available templatedin config directory`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var config []models.Template
+			for _, key := range viper.AllKeys() {
+				template := models.Template{Name: key, Path: viper.Get(key).(string)}
+				config = append(config,template)
+			}
+		utils.TemplatetoTable(config)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
